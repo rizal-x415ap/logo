eaf6240ea2c64052df5d521293087d9ace46a870
rizal-x415ap <117706231+rizal-x415ap@users.noreply.github.com>
2022-12-01 11:16:31 +0700

    Update link-shorten.js

--- link-shorten.js
+++ link-shorten.js
@@ -1151,7 +1151,7 @@ $(document).ready(function() {
     var request = !1;
     wcGetLink.click(function() {
         0 == request && (gotolinkcountdown(), request = !0), $("html, body").animate({
-            scrollTop: eval(gotolink.offset().top - 50)
+            scrollTop: eval(gotolink.offset().top - 300)
         }, 0)
     }), gotolink.on("click", function() {
         var t = aesCrypto.decrypt(convertstr($.urlParam("o")), convertstr("root"));
