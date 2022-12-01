4d6228a270249c31ef98549c0abf1719d7973142
rizal-x415ap <117706231+rizal-x415ap@users.noreply.github.com>
2022-12-01 11:10:29 +0700

    Update link-shorten.js

--- link-shorten.js
+++ link-shorten.js
@@ -1,4 +1,3 @@
-//safelink blogger di bagikan gratis oleh https://blog.choipanwendy.com versi 1.1
 $(".wcSafeShow").click(function() {
     $(".safeWrap").fadeIn()
 }), $(".wcSafeClose").click(function() {
@@ -1152,7 +1151,7 @@ $(document).ready(function() {
     var request = !1;
     wcGetLink.click(function() {
         0 == request && (gotolinkcountdown(), request = !0), $("html, body").animate({
-            scrollTop: eval(gotolink.offset().top - 10)
+            scrollTop: eval(gotolink.offset().top - 50)
         }, 500)
     }), gotolink.on("click", function() {
         var t = aesCrypto.decrypt(convertstr($.urlParam("o")), convertstr("root"));
